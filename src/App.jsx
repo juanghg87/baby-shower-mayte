0a4c5ffa342f4c94cdda37367b69c92104f8bc6e
Juan Guillermo Hernández <juanghg@gmail.com>
2025-07-07 20:38:12 -0500

    Se agregan los títulos y las imágenes iniciales

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.scss'
+import Name from "./components/name/Name.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Name />
     </>
   )
 }
---
+++ src/components/name/Name.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./name.scss";
+import Nube3 from "../../assets/nube3.png";
+import Osito2 from "../../assets/osito2.png";
+import Osito1 from "../../assets/osita1.png";
+import Nube2 from "../../assets/nube2.png";
+
+const name = () => {
+  return (
+    <div>
+      <img src={Nube3} alt="Nube decorativa" className="nube3" />
+      <img src={Osito2} alt="Osita decorativa" className="osito2" />
+      <div className="container">
+        <h2 className="message">
+          Bienvenidos al Baby Shower de
+        </h2>
+        <h1 className="name">
+          Mayté
+        </h1>
+      </div>
+      <img src={Nube2} alt="Osita decorativa" className="nube2" />
+      <img src={Osito1} alt="Osita decorativa" className="osito1" />
+    </div>
+  )
+}
+
+export default name

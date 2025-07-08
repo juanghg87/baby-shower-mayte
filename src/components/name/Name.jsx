cfb00aef48b4c8151bf923d7aec81855d12de3d1
Juan Guillermo Hernández <juanghg@gmail.com>
2025-07-08 09:54:25 -0500

    animación a componentes y botón con efecto liquid glass

--- src/components/name/Name.jsx
+++ src/components/name/Name.jsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { motion, scale } from 'framer-motion';
 import "./name.scss";
 import Nube3 from "../../assets/nube3.png";
 import Osito2 from "../../assets/osito2.png";
@@ -18,6 +18,21 @@ const Name = () => {
     }
   };
 
+  const ositaAnimation = {
+    animate: {
+      y: ["0px", "-15px", "0px"],
+      scale: [1, 1.1, 1],
+      rotate: ["0deg", "5deg", "0deg"]
+    },
+    transition: {
+      duration: 5,
+      repeat: Infinity,
+      repeatType: "mirror",
+      ease: "easeInOut",
+      times: [0, 0.5, 1]
+    }
+  }
+
   return (
     <div>
       <motion.img
@@ -26,22 +41,28 @@ const Name = () => {
         className="nube3"
         {...nubeAnimation}
       />
-      
-      <img src={Osito2} alt="Osita decorativa" className="osito2" />
-      
+
+      <motion.img
+        src={Osito2}
+        alt="Osita decorativa"
+        className="osito2"
+        {...ositaAnimation}
+      />
+
       <div className="container">
         <h2 className="message">Bienvenidos al Baby Shower de</h2>
         <h1 className="name">Mayté</h1>
       </div>
-      
+
       <motion.img
         src={Nube2}
         alt="Nube decorativa"
         className="nube2"
         {...nubeAnimation}
       />
-      
+
       <img src={Osito1} alt="Osita decorativa" className="osito1" />
+      <button id="btn" class="glass">Inivitación</button>
     </div>
   )
 }

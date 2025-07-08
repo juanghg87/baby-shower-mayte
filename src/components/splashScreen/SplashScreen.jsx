26eac8f9b6365e23a454424e838d06cd6edfc8bb
Juan Guillermo Hernández <juanghg@gmail.com>
2025-07-07 22:15:49 -0500

    mejora del splashscreen

--- src/components/splashScreen/SplashScreen.jsx
+++ src/components/splashScreen/SplashScreen.jsx
@@ -4,7 +4,29 @@ import "./SplashScreen.scss";
 const SplashScreen = () => {
     return (
         <div className="splash-screen">
-            <h1>Mayté</h1>
+            <div className="logo-container">
+                <div className="animated-logo">
+                    <div className="logo-circle">
+                        <div className="inner-circle"></div>
+                    </div>
+                    <div className="particles">
+                        {[...Array(8)].map((_, i) => (
+                            <div key={i} className="particle" style={{
+                                '--i': i,
+                                '--size': `${Math.random() * 8 + 4}px`,
+                                '--delay': `${Math.random() * 0.5}s`
+                            }}></div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            
+            <div className="text-container">
+                <h1 className="logo-text">Mayté</h1>
+                <div className="loading-bar">
+                    <div className="progress"></div>
+                </div>
+            </div>
         </div>
     )
 }
